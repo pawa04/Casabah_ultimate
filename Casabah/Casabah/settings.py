cea05d608b9e298433397aa94d9723d7fafd37e0
pawa04 <127948242+pawa04@users.noreply.github.com>
2023-07-31 23:40:33 +0200

    Update settings.py

--- Casabah/Casabah/settings.py
+++ Casabah/Casabah/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zu3&sc!-i_bae!5mzjn@3t-lw3j5ir#4rt*o4t#w-ktq8opzu3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["16.170.146.140"]
 
