cfb77ac38ed755f55bab3c1b7ba0aafcb8794fa4
pawa04 <127948242+pawa04@users.noreply.github.com>
2023-08-01 11:18:39 +0200

    Update urls.py

--- Casabah/product/urls.py
+++ Casabah/product/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [path("", views.index, name="home-page"),
                ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #hello
-urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
+
 
 
